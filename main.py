e66f84fde6726916831c9f77ebe14d00937b85b1
Sanjay4141 <145110190+Sanjay4141@users.noreply.github.com>
2023-09-26 18:20:39 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+#3.1 Write a function called linear_search_product that takes the list of products and a target product name as input. The function should perform a linear search to find the target product in the list and return a list of indices of all occurrences of the product if found, or an empty list if the product is not found.
 
-user_input = input("Enter an integer: ")
-n = int(user_input)
-if n < 0:
-    print("Please enter a non-negative integer.")
+def linear_search_product(product_list, target_product):
+
+    indices = []
+
+    for i, product in enumerate(product_list):
+
+        if product == target_product:
+
+            indices.append(i)
+
+    return indices
+
+
+
+# Example usage:
+
+products = ["apple", "banana", "apple", "orange", "apple"]
+
+target = "apple"
+
+result = linear_search_product(products, target)
+
+if result:
+
+    print(f"The product '{target}' was found at indices: {result}")
+
 else:
-    result = factorial(n)
-    print(f"The factorial of {n} is {result}")
 
+    print(f"The product '{target}' was not found in the list.")
